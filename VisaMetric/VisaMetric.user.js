c2a33ef15c43053cbba339cadfd07197d03d2ff9
FailedTech <failedtech69@gmail.com>
2023-09-11 14:47:39 +0330

    09/11/2023

--- VisaMetric/VisaMetric.user.js
+++ VisaMetric/VisaMetric.user.js
@@ -7,30 +7,30 @@
 // @author      FailedTech
 // @description 09/11/2023, 13:00:00 PM
 // @icon        https://www.visametric.com/front/images/common/favicon.png
-// @homepageURL https://github.com/FailedTech/ViolentMonkey
-// @updateURL   https://github.com/FailedTech/ViolentMonkey/raw/main/VisaMetric.user.js
-// @downloadURL https://github.com/FailedTech/ViolentMonkey/raw/main/VisaMetric.user.js
+// @homepageURL https://github.com/FailedTech/ViolentMonkey/tree/main/VisaMetric
+// @updateURL   https://github.com/FailedTech/ViolentMonkey/raw/main/VisaMetric/VisaMetric.user.js
+// @downloadURL https://github.com/FailedTech/ViolentMonkey/raw/main/VisaMetric/VisaMetric.user.js
 // @require     https://code.jquery.com/jquery-3.6.0.min.js
 // @run-at      document-idle
 // ==/UserScript==
 
-(() => {
+(async () => {
     let pathName = window.location.pathname;
-    let home = () => { $("#nationalWorkingBtn").trigger("click"); }
-    let nationalWorking = () => { $("#result1, #result3, #btnSubmit").trigger("click"); }
+    let home = await (() => { $("#nationalWorkingBtn").trigger("click"); })();
+    let nationalWorking = await (() => { $("#result1, #result3, #btnSubmit").trigger("click"); })();
     let subdirList = {
-        '/en|/en/|/en/home|/en/home/|/ir|/ir/|/ir/home|/ir/home/': {
-            home: () => { console.log('subdirList => current page is: ', pathName); home(); }
-        },
-        '/en/NationalWorking|/en/NationalWorking/|/ir/NationalWorking|/ir/NationalWorking/': {
-            nationalWorking: () => {
-                console.log('subdirList => current page is: ', pathName); nationalWorking();
-            }
-        },
-        '/en/appointment-form|/en/appointment-form/|/ir/appointment-form|/ir/appointment-form/': {
-            appointmentForm: () => {
-                console.log('subdirList => current page is: ', pathName); findActiveNavTab();
-            }
+        '/en|/en/|/en/home|/en/home/|/ir|/ir/|/ir/home|/ir/home/': () => {
+            console.log('subdirList => current page is: ', pathName); home();
+        }
+        ,
+        '/en/NationalWorking|/en/NationalWorking/|/ir/NationalWorking|/ir/NationalWorking/': () => {
+            console.log('subdirList => current page is: ', pathName); nationalWorking();
+        }
+        ,
+        '/en/appointment-form|/en/appointment-form/|/ir/appointment-form|/ir/appointment-form/': () => {
+            console.log('subdirList => current page is: ', pathName)
         }
     };
+    let matchedSubdir = Object.keys(subdirList).find(key => key.split('|').find(path => path === pathName));
+    matchedSubdir ? subdirList[matchedSubdir]() : console.log('No matching url:', pathName);
 })();
